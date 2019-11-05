7dcbd875cadb656f70eb2c4ef06b89da315e05c1
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-11-05 23:54:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailAddressParser
   end
   
   def parse
-    emails.split.collect.uniq do |email|
+    emails.split.collect.flatten.uniq do |email|
       email.flatten.uniq.split(" ")
     end
   end
